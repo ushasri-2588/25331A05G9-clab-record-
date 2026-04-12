8dadefeeb7a20bffe8a639489a139f9f990cd755
ushasri-2588 <satyaushasrilakshmimallajosyul@gmail.com>
2026-04-12 16:18:08 +0530

    Create Nested Structure.c

---
+++ Nested Structure.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct student
+{
+    int id;
+    char name[20];
+    float marks;
+};
+int main()
+{
+    struct student s[3];
+    int i;
+    for(i = 0; i < 3; i++)
+    {
+        printf("Enter ID, Name and Marks:\n");
+        scanf("%d %s %f", &s[i].id, s[i].name, &s[i].marks);
+    }
+    printf("\nStudent Details\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("ID = %d  Name = %s  Marks = %.2f\n",
+        s[i].id, s[i].name, s[i].marks);
+    }
+    return 0;
+}
